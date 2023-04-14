6d6eafacd43c3ff0ae97266897b2aecc488d4de0
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-14 03:05:50 -0300

    Add documentation

--- modules/linkedList.js
+++ modules/linkedList.js
@@ -1,7 +1,16 @@
 import Node from "./listNode.js";
 
+/**
+ * Creates a linked list object with methods to manipulate the list.
+ * @returns {LinkedList} A linked list object.
+ */
 const LinkedList = () => {
     let list = null;
+    
+  /**
+   * Appends a value to the end of the list.
+   * @param {any} value - The value to append.
+   */
   
     const append = (value) => {
       if(!list){
@@ -14,7 +23,10 @@ const LinkedList = () => {
         tmp.nextNode = Node(value);
       }
     }
-
+ /**
+   * Prepends a value to the beginning of the list.
+   * @param {any} value - The value to prepend.
+   */
     const prepend = (value) => {
       if(!list){
         list = Node(value);
@@ -22,9 +34,14 @@ const LinkedList = () => {
         list = Node(value, list)
       }
     }
-
+ /**
+   * Returns the size of the list.
+   * @returns {string} A message with the size of the list.
+   */
     const size = () => {
+      if(!list) return "List does not exist";
       let temp = list;
+      
       let count = 0;
       while(temp != null){
         count += 1;
@@ -33,12 +50,21 @@ const LinkedList = () => {
       return `The size of list is ${count}`;
     }
 
+      /**
+   * Returns the value of the head node of the list.
+   * @returns {string} A message with the value of the head node or an error message if the list does not exist.
+   */
+
     const head = () => {
       if(!list){
         return "List does not exist";
       }
       return `The value of the headNode is ${list.value}`;
     }
+      /**
+   * Returns the value of the tail node of the list.
+   * @returns {string} A message with the value of the tail node or an error message if the list does not exist.
+   */
 
     const tail = () => {
       if(!list) return "List does not exist";
@@ -47,7 +73,11 @@ const LinkedList = () => {
         temp = temp.nextNode
       }
       return `The value of the tailNode is ${temp.value}`
-    }
+    }  /**
+    * Returns the value of a node at a given index in the list.
+    * @param {number} index - The index to look up.
+    * @returns {string} A message with the value of the node or an error message if the index is invalid or the list does not exist.
+    */
 
     const at = (index) => {
       if(!list) return "List does not exist";
@@ -63,7 +93,9 @@ const LinkedList = () => {
       }
       return `The value at index ${index} is ${temp.value}`;
     }
-
+  /**
+   * Removes the last node from the list.
+   */
     const pop = () => {
       if(!list) return "List does not exist";
       let current = list;
@@ -74,7 +106,11 @@ const LinkedList = () => {
       }
       prev.nextNode = null;
     }
-
+  /**
+   * Checks if the list contains a given value.
+   * @param {any} value - The value to search for.
+   * @returns {boolean} True if the value is found, false otherwise.
+   */
     const contains = (value) => {
       if(!list) return "List does not exist";
       let temp = list;
@@ -89,7 +125,11 @@ const LinkedList = () => {
       if(i === false){return false};
       if(i === true) {return true};
     }
-
+  /**
+   * Finds the index of a node with a given value in the list.
+   * @param {any} value - The value to search for.
+   * @returns {string} A message with the index of the node or an error message if the value is not found or the list does not exist.
+   */
     const find = (value) => {
       if(!list) return "List does not exist";
       let temp = list;
@@ -107,7 +147,10 @@ const LinkedList = () => {
       }
       return `The Value ${value} do not exist in the list`;
     }
-
+  /**
+   * Converts the list to a string representation.
+   * @returns {string} A string with the values of the nodes separated by arrows and ending with null.
+   */
     const toString = () => {
       if(!list) return "List does not exist";
       let temp = list;
@@ -120,6 +163,11 @@ const LinkedList = () => {
       return stringList + 'null ';
     }
 
+  /**
+   * Inserts a value at a given index in the list.
+   * @param {any} value - The value to insert.
+   * @param {number} index - The index to insert at.
+   */
     const insertAt = (value, index) => {
 
       if(!list) return "List does not exist";
@@ -145,6 +193,12 @@ const LinkedList = () => {
       prev.nextNode = temp;
     }
 
+      /**
+   * Removes a node at a given index from the list.
+   * @param {number} index - The index to remove at.
+   * @returns {string} A message with the result of the operation or an error message if the index is invalid.
+   */
+
     const removeAt = (index) => {
       let length = Number(size().slice(-1)); //extract the length for size text
       if(index > length) return "The index is greater than the list's length";
@@ -164,7 +218,7 @@ const LinkedList = () => {
     }
 
     const getList = () => list;
-
+// Returns an object with methods to access and manipulate the list
     return {
       append,
       prepend,
--- modules/listNode.js
+++ modules/listNode.js
@@ -1,3 +1,11 @@
+/**
+ * Creates a node object with a value and a reference to the next node.
+ * @param {any} value - The value of the node.
+ * @param {Node} [nextNode=null] - The next node in the list.
+ * @returns {Node} A node object.
+ */
+
+
 const Node = (value = null, nextNode = null) => {
     return {
       value,
